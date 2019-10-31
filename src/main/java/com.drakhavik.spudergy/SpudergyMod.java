7b7832760fa6bf634afe10b588a6646f22c687ef
Adam McCoy <truedawg3355@gmail.com>
2019-10-31 19:07:15 -0400

    Git Add

--- src/main/java/com.drakhavik.spudergy/SpudergyMod.java
+++ src/main/java/com.drakhavik.spudergy/SpudergyMod.java
@@ -1,4 +1,8 @@
-@MOD(MOD_ID)
+package com.drakhavik.spudergy;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SpudergyMod.MOD_ID)
 public class SpudergyMod {
-    public static final String MOD_ID = "spudergy"
+    public static final String MOD_ID = "spudergy";
 }
